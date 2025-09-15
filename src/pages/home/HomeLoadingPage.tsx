f9f4aa60e5a48fed5848809c15e0cf7c3c3e7742
An Thanh Le <an.thanhle@infodation.vn>
2025-09-15 14:31:12 +0700

    feat: add loading state handling in HomePage; implement HomeLoadingPage component for improved user experience

---
+++ src/pages/home/HomeLoadingPage.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export default function HomeLoadingPage() {
+  const [dots, setDots] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + '.' : ''));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center skeleton">
+      <div className="flex">
+        <span className="text-xl font-semibold animate-pulse">Loading Library{dots}</span>
+      </div>
+      <progress className="progress progress-no-rounded w-[50%]"></progress>
+    </div>
+  );
+}
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,11 +1,29 @@
+import React from 'react';
+import { useContextSelector } from 'use-context-selector';
+
+import { LauncherContext } from '~/providers/LauncherProvider';
+
+import HomeLoadingPage from './HomeLoadingPage';
 import HomeNotePage from './HomeNotePage';
 import HomeSidePage from './HomeSidePage';
 
 export default function HomePage() {
+  const findReleaseNotesQuery = useContextSelector(LauncherContext, (v) => v.findReleaseNotesQuery);
+  const findAllVersionQuery = useContextSelector(LauncherContext, (v) => v.findAllVersionQuery);
+  const findAllInstanceQuery = useContextSelector(LauncherContext, (v) => v.findAllInstanceQuery);
+
+  const isLoading = findReleaseNotesQuery.isLoading || findAllVersionQuery.isLoading || findAllInstanceQuery.isLoading;
+
   return (
     <div className="flex h-full">
-      <HomeSidePage className="flex-1/4" />
-      <HomeNotePage className="flex-3/4" />
+      {isLoading ? (
+        <HomeLoadingPage />
+      ) : (
+        <React.Fragment>
+          <HomeSidePage className="flex-1/4" />
+          <HomeNotePage className="flex-3/4" />
+        </React.Fragment>
+      )}
     </div>
   );
 }

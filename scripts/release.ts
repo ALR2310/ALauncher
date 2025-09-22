4da582d40cac328088b405a6831ab5a4b3625056
An Thanh Le <an.thanhle@infodation.vn>
2025-09-22 17:46:35 +0700

    fix(release): build updater and copy executable

--- scripts/release.ts
+++ scripts/release.ts
@@ -153,6 +153,13 @@ async function buildRelease(version: string): Promise<BuildResult> {
   tauriConf.version = version;
   fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConf, null, 2));
 
+  // build updater
+  execSync('cd src-tauri && cargo build --release --bin updater', { stdio: 'inherit' });
+  fs.copyFileSync(
+    path.join('src-tauri', 'target', 'release', 'updater.exe'),
+    path.join('src-tauri', 'binaries', 'updater-x86_64-pc-windows-msvc.exe'),
+  );
+
   // clean old bundle + build
   fs.removeSync(bundleDir);
   execSync('yarn build', { stdio: 'inherit' });
